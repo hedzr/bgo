762d4610e33858887f8955a4fb38426f1cddc6b1
hz <hedzrz@gmail.com>
2022-02-02 11:40:40 +0800

    cmdr global pre-action: verbose info for debugging

--- cli/bgo/cmdr/root_cmd.go
+++ cli/bgo/cmdr/root_cmd.go
@@ -2,6 +2,7 @@ package cmdr
 
 import (
 	"github.com/hedzr/bgo/internal/logic"
+	"github.com/hedzr/bgo/internal/logic/logx"
 	"github.com/hedzr/cmdr"
 	"github.com/hedzr/cmdr/conf"
 )
@@ -12,12 +13,16 @@ func buildRootCmd() (rootCmd *cmdr.RootCommand) {
 		ver = conf.Version
 	}
 	root := cmdr.Root(appName, ver).
-		//AddGlobalPreAction(func(cmd *cmdr.Command, args []string) (err error) {
-		//	// fmt.Printf("# global pre-action 1, curr-dir: %v\n", cmdr.GetCurrentDir())
-		//	// cmdr.Set("enable-ueh", true)
-		//	err = internal.App().Init(cmd, args) // App() will be auto-closed
-		//	return
-		//}).
+		AddGlobalPreAction(func(cmd *cmdr.Command, args []string) (err error) {
+			// fmt.Printf("# global pre-action 1, curr-dir: %v\n", cmdr.GetCurrentDir())
+			// cmdr.Set("enable-ueh", true)
+			//err = internal.App().Init(cmd, args) // App() will be auto-closed
+			logx.Verbose("VERBOSE: %v, DEBUG: %v\n", logx.ToDim("%v", cmdr.GetVerboseMode()), logx.ToDim("%v", cmdr.GetDebugMode()))
+			logx.Verbose("Config File: %v\n", logx.ToDim("%v", cmdr.GetUsedConfigFile()))
+			logx.Verbose("Config File (2ndry): %v\n", logx.ToDim("%v", cmdr.GetUsedSecondaryConfigFile()))
+			logx.Verbose("Config File (alter): %v\n", logx.ToDim("%v", cmdr.GetUsedAlterConfigFile()))
+			return
+		}).
 		//AddGlobalPreAction(func(cmd *cmdr.Command, args []string) (err error) {
 		//	//fmt.Printf("# global pre-action 2, exe-path: %v\n", cmdr.GetExecutablePath())
 		//	return
