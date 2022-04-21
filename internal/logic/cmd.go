7c1f4971460711d05f824782757617734c3e9f72
hz <hedzrz@gmail.com>
2022-04-21 14:41:35 +0800

    fea: new cmd 'sbom'

--- internal/logic/cmd.go
+++ internal/logic/cmd.go
@@ -1,7 +1,11 @@
 package logic
 
 import (
+	"debug/buildinfo"
+	"fmt"
+
 	"github.com/hedzr/cmdr"
+	"gopkg.in/hedzr/errors.v3"
 )
 
 func AttachToCmdr(root *cmdr.RootCmdOpt) {
@@ -26,6 +30,48 @@ func AttachToCmdr(root *cmdr.RootCmdOpt) {
 	cmdrSubCmdInit(root)
 	cmdrSubCmdBuild(root)
 	cmdrSubCmdList(root)
+
+	cmdrSubCmdSBOM(root)
+}
+
+func cmdrSubCmdSBOM(root cmdr.OptCmd) {
+	sbom := cmdr.NewSubCmd().Titles("sbom", "s").
+		Description("show SBOM of executable", `
+			show SBOM of executable.
+			
+			Just like 'go version -m' but no need to install Go Runtime.`).
+		Action(sbomAction).
+		TailPlaceholder("<executables...>").
+		AttachTo(root)
+
+	cmdr.NewBool().
+		Titles("more", "m").
+		Description("more details", "").
+		// ToggleGroup("").
+		Group("").
+		VendorHidden(true).
+		AttachTo(sbom)
+}
+
+func sbomAction(cmd *cmdr.Command, args []string) (err error) {
+	var ec = errors.New("processing executables...")
+	for _, file := range args {
+		ec.Attach(sbomOne(file))
+	}
+	return
+}
+
+func sbomOne(file string) (err error) {
+	var inf *buildinfo.BuildInfo
+	if inf, err = buildinfo.ReadFile(file); err != nil {
+		return
+	}
+
+	fmt.Printf("SBOM of %q\n%#v\n\n", file, inf)
+	for _, d := range inf.Deps {
+		fmt.Printf("%#v\n", *d)
+	}
+	return
 }
 
 const gnBuild = "Build Target"
